84e9eefd36d650d2613d422382554095ce7c47ed
Brandon Roberts <brandonlroberts@outlook.com>
2022-08-24 10:26:01 -0400

    Add project files.

---
+++ MyBlazor.Hybrid/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using MyBlazor.Hybrid.Services;
+
+namespace MyBlazor.Hybrid
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            // If using localhost
+            builder.Services.AddDevHttpClient(7030);
+#else 
+            // If using a published development API instead of localhost
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("Production API") });
+#endif
+            builder.Services.AddScoped<IFetchDataService, FetchDataService>();
+
+            return builder.Build();
+        }
+    }
+}
